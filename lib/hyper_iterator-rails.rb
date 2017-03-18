7a1f211b00caadaf7d28c3b2d945fcaf720c6dce
Edmund Li <edmund.xz.lee@gmail.com>
2017-03-18 12:11:13 -0700

    load dependency automatically

--- lib/hyper_iterator-rails.rb
+++ lib/hyper_iterator-rails.rb
@@ -3,5 +3,8 @@ require_relative './hyper_iterator/rails/active_record'
 
 module HyperIterator
   module Rails
+    class Railtie < ::Rails::Railtie
+      require 'hyper_iterator'
+    end
   end
 end
